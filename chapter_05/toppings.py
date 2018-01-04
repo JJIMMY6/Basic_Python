e6104445c5daac498a7a9c4d56ac24be7b2c9a81
jimmy <jjimmychen6@outlook.com>
2018-01-04 20:00:01 +0800

    banned_ users

--- chapter_05/toppings.py
+++ chapter_05/toppings.py
@@ -1,4 +1,5 @@
 requested_topping = 'mushrooms'
 
+# Combine an exclamation point and an equal sign (!=)
 if requested_topping != 'anchovies':
 	print("Hold the anchovies!")
--- chapter_05/untitled.py
+++
@@ -1,5 +0,0 @@
-requested_topping = 'mushrooms'
-
-# Combine an exclamation point and an equal sign (!=)
-if requested_topping != 'anchovies':
-	print("Hold the anchovies!")
