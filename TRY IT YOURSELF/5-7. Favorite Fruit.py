327e7990f21a730e50bc421ee642bf4fc29df4d9
jimmy <jjimmychen6@outlook.com>
2018-01-06 16:36:36 +0800

    try it yourself

---
+++ TRY IT YOURSELF/5-7. Favorite Fruit.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['apples', 'bananas', 'cherry']
+
+if 'apples' in favorite_fruits:
+	print("You really like apple.")
+if 'bananas' in favorite_fruits:
+	print("You really like bananas.")
+if 'peaches' in favorite_fruits:
+	print("You really like peaches.")
+if 'blueberries' in favorite_fruits:
+	print("You really like blueberries.")
+if 'melons' in favorite_fruits:
+	print("You really like melons.")
