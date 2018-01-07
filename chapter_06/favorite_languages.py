c9616dad178a34384c2ea19903200cc0ebd837eb
jimmy <jjimmychen6@outlook.com>
2018-01-07 20:48:08 +0800

    add code from chapter_06

---
+++ chapter_06/favorite_languages.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+	'jen': 'python',
+	'sarah': 'c',
+	'edward': 'ruby',
+	'phil': 'python',
+	}
+
+print("Sarah's favorite languages is " + 
+	favorite_languages['sarah'].title() + 
+	".")
+
