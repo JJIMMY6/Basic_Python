ac1569bdcd72b9075f8ff843f8bd03556ff2e411
jimmy <jjimmychen6@outlook.com>
2018-01-09 23:30:20 +0800

    try it yourself

---
+++ TRY IT YOURSELF/6-1. Person.py
@@ -0,0 +1,11 @@
+person = {
+	'first_name': 'jimmy',
+	'last_name': 'chen',
+	'age': 23,
+	'city': 'shanghai'
+	}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
