b4c784659615f5b75a92b796536c5d1b345e7cff
jimmy <jjimmychen6@outlook.com>
2018-01-10 17:00:13 +0800

    try it yourself

---
+++ TRY IT YOURSELF/6-2. Favorite Numbers.py
@@ -0,0 +1,14 @@
+favorite_numbers = {
+	'neal': 3,
+	'anna': 6,
+	'robin': 1,
+	}
+
+num = favorite_numbers['neal']
+print("Neal's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['anna']
+print("Anna's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['robin']
+print("Robin's favorite number is " + str(num) + ".")
---
+++ TRY IT YOURSELF/6-3. Glossary.py
@@ -0,0 +1,14 @@
+glossary = {
+	'list': 'A list is a collection of items in a particular order. ',
+	'dictionary': 'A dictionary is a collection of key-value pairs. ',
+	'string': 'A series of characters.'
+}
+
+word = 'list'
+print("\n" + word + ": " + glossary[word])
+
+word = 'dictionary'
+print("\n" + word + ": " + glossary[word])
+
+word = 'string'
+print("\n" + word + ": " + glossary[word])
