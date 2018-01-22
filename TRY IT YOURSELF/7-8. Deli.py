3211c174ee10469369857d3c82e906532d320038
jimmy <jjimmychen6@outlook.com>
2018-01-22 18:06:20 +0800

    try it yourself

---
+++ TRY IT YOURSELF/7-8. Deli.py
@@ -0,0 +1,8 @@
+sandwich_orders = ['beef', 'chicken', 'cheese']
+finsined_sandwiched = []
+
+for sandwich in sandwich_orders:
+	print("I made your " + sandwich + " sandwich.")
+	finsined_sandwiched.append(sandwich)
+
+print("\n")
