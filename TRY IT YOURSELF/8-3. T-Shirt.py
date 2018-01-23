41e256f0837a0b3dd95ed3ead13066272a88f027
jimmy <jjimmychen6@outlook.com>
2018-01-23 20:46:30 +0800

    try it yourself

---
+++ TRY IT YOURSELF/8-3. T-Shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+	"""Summarize the shirt."""
+	print("\nI'm going to make " + size + " t-shirt.")
+	print('It will say, "' + message + '"')
+
+make_shirt("small", "life is short.")
+make_shirt("large", "use python.")
