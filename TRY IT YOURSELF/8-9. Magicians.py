cfd63592eff5aaa7bed60342e124fb4651b67b8d
jimmy <jjimmychen6@outlook.com>
2018-01-27 23:26:33 +0800

    try it yourself

---
+++ TRY IT YOURSELF/8-9. Magicians.py
@@ -0,0 +1,9 @@
+def show_magicians(magicians):
+	"""Print the name of each magician in the list."""
+	for magician in magicians:
+		print(magician.title())
+
+
+magicians =['David Copperfield', 'Criss Angel']
+
+show_magicians(magicians)
