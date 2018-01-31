43890cf835e743aaa566807c4e21785c6369444f
jimmy <jjimmychen6@outlook.com>
2018-01-31 23:05:52 +0800

    add code from chapter_09

---
+++ chapter_09/dog_2.py
@@ -0,0 +1,22 @@
+# Creating the Dog Class
+class Dog():
+	"""A simple attempt to model a dog."""
+
+	def __init__(self, name, age):
+		"""Initialize name and age attributes."""
+		self.name = name
+		self.age = age
+
+	def sit(self):
+		"""Simulate a dog sitting in response to a command."""
+		print(self.name.title() + " is now sitting.")
+
+	def roll_over(self):
+		"""Simulate rolling over in respone to a command."""
+		print(self.name.title() + " rolled over!")
+
+my_dog = Dog('willie', 6)
+
+# Calling Methods
+my_dog.sit()
+my_dog.roll_over()
---
+++ chapter_09/dog_3.py
@@ -0,0 +1,31 @@
+# Creating the Dog Class
+class Dog():
+	"""A simple attempt to model a dog."""
+
+	def __init__(self, name, age):
+		"""Initialize name and age attributes."""
+		self.name = name
+		self.age = age
+
+	def sit(self):
+		"""Simulate a dog sitting in response to a command."""
+		print(self.name.title() + " is now sitting.")
+
+	def roll_over(self):
+		"""Simulate rolling over in respone to a command."""
+		print(self.name.title() + " rolled over!")
+
+# Creating Multiple Instances
+my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
+
+# Accessing Attributes
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
+my_dog.sit()
+
+print("\nYour dog's name is " + your_dog.name.title() + ".")
+print("Your dog is " + str(your_dog.age) + " years old.")
+your_dog.sit()
+
+
