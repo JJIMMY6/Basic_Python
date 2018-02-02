a6a0329977d8e3f1b61250eeeba1b430e7f772fc
jimmy <jjimmychen6@outlook.com>
2018-02-02 20:58:05 +0800

    try it yourself

---
+++ TRY IT YOURSELF/9-2. Three Restaurants.py
@@ -0,0 +1,29 @@
+class Restaurant():
+	"""A class to describe an Restaurant"""
+
+	def __init__(self, name, cuisine_type):
+		"""Initialize name and cuisine_type attributes."""
+		self.name = name
+		self.cuisine_type = cuisine_type
+
+	def describe_restaurant(self):
+		""""""
+		print(self.name.title() + " provide " + self.cuisine_type.title())
+
+	def open_restaurant(self):
+		"""Prints a message that restaurant is open."""
+		print(self.name.title() + " is open.")
+
+
+restaurant = Restaurant('fairmont peace hotel', 'chinese food')
+restaurant.describe_restaurant()
+
+restaurant_1 = Restaurant('dahuchun', 'shanghai food')
+restaurant_1.describe_restaurant()
+
+restaurant_2 = Restaurant('pistolera', 'mexico food')
+restaurant_2.describe_restaurant()
+
+
+
+	
